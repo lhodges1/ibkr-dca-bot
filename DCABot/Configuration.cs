889a1d0c548c0088dbb55d260fefead57b7443e7
lhodges1 <lachiehodges7785@gmail.com>
2022-08-30 23:45:31 +1000

    first commit

---
+++ DCABot/Configuration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace DCABot
+{
+    public class Configuration
+    {
+        public struct InteractiveBrokerSettings
+        {
+            public string APIKey { get; set; }
+            public bool paperTrading { get; set; }
+        }
+
+        public struct Trades
+        {
+            public string asset { get; set; }
+            public double orderQty { get; set; }
+            public string currency { get; set; }
+            public string schedule { get; set; }
+        }
+    }
+}
---
+++ DCABot/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Serilog;
+using Microsoft.Extensions.Configuration;
+
+namespace Program
+{
+    class Program
+    {
+        private IConfiguration Configuration;
+        public static void Main(string[] args)
+        {
+            //Configure logger
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("logs.txt")
+                .CreateLogger();
+
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddInMemoryCollection("appsettings.json");
+        }
+    }
+}
